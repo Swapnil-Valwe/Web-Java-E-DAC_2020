0d17d2e4104c290092ddc24ad604d53522a05bb4
Swapnil-Valwe <72023075+Swapnil-Valwe@users.noreply.github.com>
2021-02-18 23:51:21 +0530

    Add files via upload

---
+++ second/src/second/Second.java
@@ -0,0 +1,7 @@
+package second;
+
+public class Second {
+public static void main(String[] args) {
+	System.out.println("Second Program");
+}
+}
---
+++ third/src/third/Third.java
@@ -0,0 +1,7 @@
+package third;
+
+public class Third {
+public static void main(String[] args) {
+	System.out.println("Third Program");
+}
+}
